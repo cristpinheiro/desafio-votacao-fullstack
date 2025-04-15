ac5a464d376e430b85fbd1b8c335cc822641edee
Cristiano <cristianopinheiro2011@gmail.com>
2025-04-15 03:00:41 -0300

    add backend project

---
+++ cooperavoteback/src/main/java/com/cooperavote/cooperavoteback/CooperaVoteBackApplication.java
@@ -0,0 +1,13 @@
+package com.cooperavote.cooperavoteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperaVoteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperaVoteBackApplication.class, args);
+	}
+
+}
---
+++ cooperavoteback/src/test/java/com/cooperavote/cooperavoteback/CooperaVoteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperavote.cooperavoteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperaVoteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
